95fa0fa0f4a53bc7fda5e2ed847fd74a8866d387
Mark Xie <ly.markxie@gmail.com>
2020-11-05 21:26:22 -0500

    updated numbers for homework

--- js/numbers.js
+++ js/numbers.js
@@ -1,13 +1,44 @@
 $(() =>{
+
+    styleChange();
     $("#plus").click(() =>{
         let holdNum = $("#num").val();
         holdNum++;
         $("#num").val(holdNum);
+        styleChange();
     });
 
     $("#minus").click(() =>{
         let holdNum = $("#num").val();
         holdNum--;
         $("#num").val(holdNum);
+        styleChange();
     });
+    $( "#num" ).change(function() {
+        styleChange();
+      });
+    function styleChange(){
+        let holdNum = $("#num").val();
+        if(holdNum % 2 == 0){
+            $("#num").css("color", "red");
+        }
+        else{
+            $("#num").css("color", "black");
+        }
+
+        if(holdNum % 3 == 0){
+            $("#num").css("font-weight", "bold");
+        }
+        else{
+            $("#num").css("font-weight", "normal");
+        }
+
+        if(holdNum % 5 == 0){
+            $("#num").css("font-style", "italic");
+        }        
+        else{
+            $("#num").css("font-style", "normal");
+        }
+    }
+    
 });
